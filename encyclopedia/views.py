bcbc3e9218b013bf3640851e3a2788c1bb451ae5
unknown <corbettjustis@gmail.com>
2020-07-07 18:28:59 -0600

    init and entry pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    
+    if not entry:
+        html = None
+    else:
+        html = Markdown().convert(entry)
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "html": html
     })
 
